87ff2dfe338b89d4bc9d7a6c02d7aed538bd3cac
jaydeep <jaydeepmangoit721@gmail.com>
2023-01-31 16:07:22 +0530

    work on items

--- Controllers/InvoiceController/itemController.js
+++ Controllers/InvoiceController/itemController.js
@@ -15,8 +15,6 @@ module.exports = {
     res.status(200).json({ message: "Item created successfully", data: item });
   },
   GetItembyid: async (req, res) => {
-    console.log(req.body.id);
-
     let id = req.body.id ? req.body.id : null;
     var sql = `SELECT id,name,price,description FROM items WHERE id IN(${id})`;
     const item = await query(sql);
@@ -24,6 +22,12 @@ module.exports = {
     res.status(200).json({ data: item });
   },
   GetItem: async (req, res) => {
+    var sql = `SELECT id,name,price,description FROM items`;
+    const item = await query(sql);
+
+    res.status(200).json({ data: item });
+  },
+  GetItemData: async (req, res) => {
     var sql = `SELECT id,name,price,description FROM items`;
     const item = await query(sql);
 
