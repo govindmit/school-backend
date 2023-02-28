2a8aac799e9661cc37567948e04149d528158d30
govind-mit <govind@mangoitsolutions.com>
2023-02-28 19:40:19 +0530

    changes in query

--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -4,7 +4,8 @@ const query = util.promisify(mysqlconnection.query).bind(mysqlconnection);
 module.exports = {
    createTransaction:async (req, res) => {
     try{
-        const { totalAmount,paidAmount,transactionId,amexorderId,paymentMethod,idForPayment} = req.body;
+        console.log("body =>",req.body);
+        const { totalAmount,paidAmount,transactionId,amexorderId,paymentMethod,idForPayment,creditNotesId} = req.body;
         if (!totalAmount || !paidAmount || !transactionId ||  !amexorderId || !paymentMethod || !idForPayment) {
            res.status(400).send({ message: "All field is required" });
            return
@@ -18,7 +19,7 @@ module.exports = {
                     res.status(201).send({message:"sales order transactionId not found !"});
                     return 
                 }
-            const transactionInsertQuery = `insert into transaction (sales_order_Id,paymentMethod ,transactionId,totalAmount,paidAmount,amex_order_Id) values(${salesOrderResponse[0].id},"${paymentMethod}","${transactionId}",${totalAmount},${paidAmount},"${amexorderId}")`
+            const transactionInsertQuery = `insert into transaction (sales_order_Id,paymentMethod ,transactionId,totalAmount,paidAmount,amex_order_Id,creditNotesId) values(${salesOrderResponse[0].id},"${paymentMethod}","${transactionId}",${totalAmount},${paidAmount},"${amexorderId}",${creditNotesId})`
             const insetTransatction = await query(transactionInsertQuery);
             res.status(200).send({message:`transaction created for sales order ${idForPayment}`})
 
@@ -30,7 +31,8 @@ module.exports = {
                 res.status(201).send({message:"invoiceId not found in invoices !"});
                 return 
             }
-            const transactionInsertQuery = `insert into transaction (invoiceId,paymentMethod ,transactionId,totalAmount,paidAmount,amex_order_Id) values(${invoiceIDQueryResponse[0].id},"${paymentMethod}","${transactionId}",${totalAmount},${paidAmount},"${amexorderId}")`
+            
+            const transactionInsertQuery = `insert into transaction (invoiceId,paymentMethod ,transactionId,totalAmount,paidAmount,amex_order_Id,creditNotesId) values(${invoiceIDQueryResponse[0].id},"${paymentMethod}","${transactionId}",${totalAmount},${paidAmount},"${amexorderId}",${creditNotesId})`
             const insetTransatction = await query(transactionInsertQuery);
             res.status(200).send({message:`transaction created for the invoice ${idForPayment}`})
    
